3c367924086a96f1cf40b71190bca291ea2fe7f3
10bits <loseher2020@outlook.com>
2020-08-05 07:53:08 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 function set_env() { echo ::set-env name=$1::$2; }
-set_env name hahah
-echo "env name:$name"
+set_env name "hahah"
+set_env git_url "https://github.com/10bits/github-actions-test"

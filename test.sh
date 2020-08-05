d0b658d9ed839ef7ce91a2a5382f7a064aac8c2b
10bits <loseher2020@outlook.com>
2020-08-05 15:27:05 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ function set_env() { echo ::set-env name=$1::$2; }
 set_env name "hahah"
 set_env git_url "https://github.com/10bits/github-actions-test"
 set_env url $1
-if [ -n "$2"]; then
+if [ -n "$2" ]; then
   if [ $2 = 'google' ]; then
     echo "谷歌 yes"
   else

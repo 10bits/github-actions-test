42b445f2b73563070d290aff526c8dcb5942a9f4
10bits <loseher2020@outlook.com>
2020-08-05 15:17:51 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,5 +4,7 @@ set_env name "hahah"
 set_env git_url "https://github.com/10bits/github-actions-test"
 set_env url $1
 if [ $2 = 'google' ]; then
-  echo "google yes"
+  echo "谷歌 yes"
+else
+  echo "百度 sb"
 fi

ae41dca57b34657c81196748878d8bfa3a29698d
10bits <loseher2020@outlook.com>
2023-01-20 22:31:24 +0800

    update

--- asmrlive.js
+++ asmrlive.js
@@ -1,1 +1,13 @@
 pp(env.get("ASMR_URL"))
+aR = ajax({ url: env.get("ASMR_URL"), baseUrl: "https://asmrs.live" }).aR()
+title = aR.getString(`title@text##【音】\[步非烟ASMR\]|在线播放|.mp3##`)
+pp(title)
+js1 = aR.getString("##var token.*##$0###")
+js2 = aR.getString("##var m3u8.*##$0###")
+m3u8 = aR.getString("@js:" + js1 + js2 + "m3u8 = m3u8+'?token='+token;m3u8", true)
+pp(m3u8)
+download = env.get("DOWNLOAD")
+if (m3u8 && download == "yes") {
+    shell.run("ffmpeg", "-i", m3u8, `/opt/catvod/output/${title}.mp3`)
+    shell.run("echo", `"UPLOAD_NAME=${title}"`, ">>", "$GITHUB_ENV")
+}

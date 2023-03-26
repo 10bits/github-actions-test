57674b64bc6472dc13540a342fd09c682f440e59
10bits <loseher2020@outlook.com>
2023-03-26 13:08:15 +0800

    update

--- asmrlive.js
+++ asmrlive.js
@@ -1,4 +1,7 @@
-pp(ajax({url:"https://www.google.com/search",body:{"q":"sehuatang","num":"22","hl":"en","start":"0"}}).html())
+pp(ajax({
+    url: "https://www.google.com/search",
+    body: "q=sehuatang&hl=en&num=22&start=0"
+}).html())
 pp(env.get("ASMR_URL"))
 aR = ajax({ url: env.get("ASMR_URL"), baseUrl: "https://asmrs.live" }).aR()
 title = aR.getString(`title@text##【音】\[步非烟ASMR\]|在线播放|.mp3##`)

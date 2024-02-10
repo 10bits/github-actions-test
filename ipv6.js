5b28cfd03029df8accdc31c815cab981eb8351aa
10bits <loseher2020@outlook.com>
2024-02-10 22:15:39 +0800

    Update and rename test.js to ipv6.js

---
+++ ipv6.js
@@ -0,0 +1,15 @@
+loadScript("jschacha20.js")
+loadScript("base64.js")
+function stringToUint8Array(str) {
+    var arr = [];
+    for (var i = 0, j = str.length; i < j; ++i) {
+        arr.push(str.charCodeAt(i));
+    }
+    var tmpUint8Array = new Uint8Array(arr);
+    return tmpUint8Array
+}
+const key=stringToUint8Array(env.get("CHACHA20KEY"))
+const nonce=stringToUint8Array(env.get("CHACHA20NONCE"))
+const message=stringToUint8Array(env.get("IPV6"))
+const encrypt = new JSChaCha20(key, nonce).encrypt(message)
+pp(Base64.fromUint8Array(encrypt))
--- test.js
+++
@@ -1,4 +0,0 @@
-url = `https://www.sehuatang.org/`
-aR = ajax({ url }).aR()
-pp(aR.getString("title@text"))
-pp(aR.getString("a"))

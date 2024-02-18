ee6fb078c2e36434a551a3ddb4e2ad9e53571305
10bits <loseher2020@outlook.com>
2024-02-18 22:18:58 +0800

    Update climages.js

--- js/climages.js
+++ js/climages.js
@@ -3,9 +3,9 @@ function replaceIllegalCharacters(fileName) {
   const replacementCharacter = "_";
   return fileName.replace(illegalCharacters, replacementCharacter);
 }
-inputs = env.get("URL")
+let inputs = env.get("URL")
 pp(inputs)
-urls = inputs.match(/https:\/\/[^:]+.html/g)
+let urls = inputs.match(/https:\/\/[^:]+.html/g)
 pp(urls)
 for (let url of urls) {
   let aR = ajax({ url }).aR()
@@ -13,7 +13,7 @@ for (let url of urls) {
   let result = aR.getString("class.tpc_cont@img@ess-data")
   pp(title)
   pp(result)
-  filename=replaceIllegalCharacters(title)
+  let filename=replaceIllegalCharacters(title)
   let output_dir=`/opt/catvod/output/"${filename}"`
   shell.run(`mkdir -p ${output_dir}`)
   saveFile(`/opt/urls.txt`, result)
